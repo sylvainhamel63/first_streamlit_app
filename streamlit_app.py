a95563220efe0a13e7b37393b810112e66239714
sylvainhamel63 <129001328+sylvainhamel63@users.noreply.github.com>
2023-03-26 14:59:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title( 'My Parents New Heathly Diner')
 
-streamlit.caption('Omega 3 & Blueberry Oatmeal')
+streamlit.caption("Omega 3 & Blueberry Oatmeal")
 
-st.caption( 'Kale, Spinach & Rocket Smoothie')
+
 
