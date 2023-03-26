724e7a875691e8bc70ffe447f8c35e469919d73e
sylvainhamel63 <129001328+sylvainhamel63@users.noreply.github.com>
2023-03-26 15:06:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe( my_fruiot_list)
+streamlit.dataframe( my_fruit_list)
 
 streamlit.title( 'My Parents New Heathly Diner')
 
